c21631079b30b02358816f06f7871fa423d12269
Donwin Lacdao <dlacdao2@academic.RRC.CA>
2025-02-28 11:03:02 -0600

    added the clients page to middleware.ts to ensure only logged in users can view the page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/user-home(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/user-home(.*)", /clients(.*)/]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (isProtectedRoute(request)) {
