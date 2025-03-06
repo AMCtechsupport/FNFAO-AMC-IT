141a05f2861afaf2b179b0071f45b68f2b08b7de
Donwin Lacdao <dlacdao2@academic.RRC.CA>
2025-03-06 08:22:46 -0600

    Manually added manager-dash files into this branch as there were too many merge conflicts. Refer to manager-dash-donwin for branch history notes

---
+++ src/app/lib/assign-advocate.js
@@ -0,0 +1,40 @@
+// lib/assign-advocate.js
+import supabase from "./supabase";
+
+// Server function to fetch clients and advocates
+export async function fetchClientsAndAdvocates() {
+  try {
+    const { data: clientsData, error: clientsError } = await supabase
+      .from("Clients")
+      .select("*");
+
+    const { data: advocatesData, error: advocatesError } = await supabase
+      .from("Advocates")
+      .select("*");
+
+    if (clientsError || advocatesError) {
+      throw new Error(clientsError?.message || advocatesError?.message);
+    }
+
+    return { clientsData, advocatesData };
+  } catch (err) {
+    throw new Error("Error fetching data: " + err.message);
+  }
+}
+
+export async function assignAdvocateToClient(client_id, advocate_id) {
+  const { data, error } = await supabase.from("Assigned Advocates").insert([
+    {
+      client_id,
+      advocate_id,
+      dateAssigned: new Date().toISOString(),
+    },
+  ]);
+
+  if (error) {
+    console.error("Error assigning advocate:", error.message);
+    return { error: error.message };
+  }
+
+  return data;
+}
