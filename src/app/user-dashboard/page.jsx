ca923fc461cb64b3153d18a91757c7718f96a0a5
Donwin Lacdao <dlacdao2@academic.RRC.CA>
2025-03-25 13:48:31 -0500

    Confirmed merge successful

--- src/app/user-dashboard/page.jsx
+++ src/app/user-dashboard/page.jsx
@@ -40,6 +40,7 @@ export default function AssignedClientsToAdvocate() {
               }`
             : "Loading..."}
         </h2>
+
         {advocate ? (
           <AssignedClientsList advocateId={advocate.advocate_id} />
         ) : (
