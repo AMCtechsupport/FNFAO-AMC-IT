8c94ee9cb95b42a0e444f1c641c35b54682d666b
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-07-07 15:53:58 -0500

    WIP: (pre-intake) Exported 'general info' and 'about you' sections to their own React-components
    Taken from preIntakeFrom.jsx into PreIntakeGeneralInfo.jsx and PreIntakeAboutYou.jsx

---
+++ src/app/pre-intake-components/PreIntakeGeneralInfo.jsx
@@ -0,0 +1,160 @@
+"use client";
+import React from "react";
+import styles from "../pre-intake/preIntake.module.css";
+import { Field, ErrorMessage} from "formik";
+import { Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PhoneNumberInput from "@/components/ValidPhoneNumber";
+import InputField from "@/components/InputField";
+import ReferredBySelect from "@/components/ReferredBySelect";
+import ProvincesSelect from "@/components/ProvincesSelect";
+
+const PreIntakeGeneralInfo = ({ errors }) => {
+    return(
+        <>
+            <Row>
+                <Col md={8} />
+                <Col md={4}>
+                    <ReferredBySelect
+                    name="referredBy"
+                    label="How did the client learn about FNFAO?"
+                    error={errors.referredBy}
+                    />
+                </Col>
+            </Row>
+            <h3 className="text-dark">General Information</h3>
+            <Row>
+            <Col md={3}>
+                <InputField
+                name="firstName"
+                label="First Name:*"
+                placeholder="John"
+                error={errors.firstName}
+                />
+            </Col>
+            <Col md={3}>
+                <InputField
+                name="middleName"
+                label="Middle Name:"
+                error={errors.middleName}
+                />
+            </Col>
+            <Col md={3}>
+                <InputField
+                name="lastName"
+                label="Last Name:*"
+                placeholder="Connor"
+                error={errors.lastName}
+                />
+            </Col>
+            <Col md={3}>
+                <div>
+                <label htmlFor="dateOfBirth">Date of Birth:*</label>
+                <Field type="date" id="dateOfBirth" name="dateOfBirth" />
+                <ErrorMessage
+                    name="dateOfBirth"
+                    component={() => (
+                    <p className={styles.errorText}>{errors.dateOfBirth}</p>
+                    )}
+                />
+                </div>
+            </Col>
+            </Row>
+            <Row>
+                <Col md={3}>
+                    <InputField
+                    name="address"
+                    label="Address:"
+                    placeholder="161 Main St, Unit 230"
+                    error={errors.address}
+                    />
+                </Col>
+                <Col md={3}>
+                    <InputField
+                    name="city"
+                    label="City:"
+                    placeholder="Winnipeg"
+                    error={errors.city}
+                    />
+                </Col>
+                <Col md={4}>
+                    <ProvincesSelect
+                    name="province"
+                    label="Province:"
+                    error={errors.province}
+                    />
+                </Col>
+                <Col md={2}>
+                    <InputField
+                    name="postalCode"
+                    label="Postal code:"
+                    placeholder="R3C 0V8"
+                    error={errors.postalCode}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col md={3}>
+                    <div>
+                    <label htmlFor="phoneNumber">Phone Number:</label>
+                    <Field
+                        type="number"
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        component={PhoneNumberInput}
+                        placeholder="(123) 456-7890"
+                    />
+                    </div>
+                </Col>
+                <Col md={4}>
+                    <div>
+                    <label htmlFor="email">Email:</label>
+                    <Field type="email" id="email" name="email" placeholder="john123@example.com" />
+                    <ErrorMessage
+                        name="email"
+                        component={() => (
+                        <p className={styles.errorText}>{errors.email}</p>
+                        )}
+                    />
+                    </div>
+                </Col>
+            </Row>
+
+            <Row className={styles.group}>
+                <h6>Emergency Contact</h6>
+                <Col>
+                    <InputField
+                    name="emergencyContactFirstName"
+                    label="First Name:"
+                    placeholder=""
+                    error={errors.emergencyContactFirstName}
+                    />
+                </Col>
+
+                <Col>
+                    <InputField
+                    name="emergencyContactLastName"
+                    label="Last Name:"
+                    placeholder=""
+                    error={errors.emergencyContactLastName}
+                    />
+                </Col>
+
+                <Col>
+                    <div>
+                    <label htmlFor="emergencyContactNumber">Phone Number:</label>
+                    <Field
+                        type="number"
+                        id="emergencyContactNumber"
+                        name="emergencyContactNumber"
+                        component={PhoneNumberInput}
+                        placeholder="(123) 456-7890"
+                    />
+                    </div>
+                </Col>
+            </Row>
+        </>
+    );
+};
+
+export default PreIntakeGeneralInfo;
