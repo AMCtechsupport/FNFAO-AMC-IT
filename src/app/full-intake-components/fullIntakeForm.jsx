abcc6d772d74a1c31128d1762298692a6e18a9d6
Craig Ian Turner (RRC_FA-2023_BIT) <cturner56@academic.rrc.ca>
2025-07-21 15:50:55 -0500

    Finished exporting general information from full-intake form

--- src/app/full-intake-components/fullIntakeForm.jsx
+++ src/app/full-intake-components/fullIntakeForm.jsx
@@ -2,16 +2,9 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "../full-intake/fullIntake.module.css";
-import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
+import { Formik, Form } from "formik";
 import { Button, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import PhoneNumberInput from "@/components/ValidPhoneNumber";
-import InputField from "@/components/InputField";
-import ProvincesSelect from "@/components/ProvincesSelect";
-import FirstNationSelect from "@/components/FirstNationSelect";
-import MartialStatusSelect from "@/components/MartialStatusSelect";
-import YesNoSelect from "../../components/yesNoSelect";
 
 import { handleNotesUpdate } from "../utils/notesUpdates"; // handles updates to the Notes table
 import { handleFamilyUpdate }  from "../utils/familyUpdates";
@@ -30,6 +23,7 @@ import LegalNotesPartition from "./form-sections/LegalNotesPartition";
 import ChildrenPartition from "./form-sections/ChildrenPartition";
 import ChildFamilyServicesPartition from "./form-sections/ChildFamilyServices";
 import GeneralInformationPartition from "./form-sections/GeneralInformationPartition";
+import GeneralInformationHeader from "./GeneralInformationHeader";
 
 export default function FullIntakeForm({client_id, userId, getToken, isEditMode = false} ){
     const router = useRouter();
@@ -494,125 +488,16 @@ export default function FullIntakeForm({client_id, userId, getToken, isEditMode 
                         <h2 className={styles.centeredTitle}>FULL-INTAKE FORM</h2>
                     </div>
                     <hr className="separator-line" />
-                        <div >
 
-                            <Row>
-                                <Col md={4}><label htmlFor="createdAt"> <strong> Created At: </strong> {values.createdAt}</label>
+                        {/* General Info Section */}
+                        <GeneralInformationHeader
+                            values={values}
+                            errors={errors}
+                            isEditing={isEditing}
+                        />
 
-                                </Col>
-
-                                <Col md={4}><label htmlFor="dateModified"> <strong> Last Updated: </strong> {values.dateModified}</label>
-
-                                </Col>
-
-                                <Col md={4}>
-                                    <label><strong>Updated by:</strong></label>
-                                    <div>{values.modifiedBy}</div>
-                                </Col>
-                            </Row>
-                        </div>
-                        <hr className="separator-line" />
-
-                        <div className="bg- p-2 rounded border border-light border-1">
-                            <Row className="mt-3">
-                                <Col md={3}>
-                                    <InputField name="firstName" label="First Name:" placeholder="John" error={errors.firstName} disabled={!isEditing} />
-                                </Col>
-
-                                <Col md={3}>
-                                    <InputField name="middleName" label="Middle Name:" error={errors.middleName} disabled={!isEditing} />
-                                </Col>
-                                <Col md={3}>
-                                    <InputField name="lastName" label="Last Name:" placeholder="Connor" error={errors.lastName} disabled={!isEditing} />
-                                </Col>
-
-                                <Col md={3}>
-                                    <div>
-                                        <label htmlFor="dateOfBirth">Birth Date:</label>
-                                        <Field type="date" id="dateOfBirth" name="dateOfBirth" disabled={!isEditing} />
-                                        <ErrorMessage name="dateOfBirth" component={() => <p className={styles.errorText}>{errors.dateOfBirth}</p>} />
-                                    </div>
-                                </Col>
-                            </Row>
-
-                            <Row >
-                                <Col md={3}>
-                                    <InputField name="address" label="Address:" placeholder="161 Main St, Unit 230" error={errors.address} disabled={!isEditing} />
-                                </Col>
-
-                                <Col md={3}>
-                                    <InputField name="city" label="City:" placeholder="Winnipeg" error={errors.city} disabled={!isEditing} />
-                                </Col>
-
-                                <Col md={4}>
-
-                                    <ProvincesSelect name="province" label="Province:" error={errors.province} disabled={!isEditing} />
-                                </Col>
-
-                                <Col md={2}>
-                                    <InputField name="postalCode" label="Postal code:" placeholder="R3C 0V8" error={errors.postalCode} disabled={!isEditing} />
-                                </Col>
-                            </Row>
-
-                            <Row className="mb-4">
-                                <Col md={3}>
-                                    <div>
-                                        <label htmlFor="phoneNumber">Phone Number:</label>
-                                        <Field type="number" id="phoneNumber" name="phoneNumber" component={PhoneNumberInput} placeholder="(123) 456-7890" disabled={!isEditing} />
-                                        <ErrorMessage name="phoneNumber" component={() => <p className={styles.errorText}>{errors.phoneNumber}</p>} />
-                                    </div>
-                                </Col>
-
-                                <Col md={4}>
-                                    <div>
-                                        <label htmlFor="email">Email:</label>
-                                        <Field type="email" id="email" name="email" disabled={!isEditing} />
-                                        <ErrorMessage name="email" component={() => <p className={styles.errorText}>{errors.email}</p>} />
-                                    </div>
-                                </Col>
-                            </Row>
-                        </div>
-                        {/* Emrgency contact */}
-                        {/* <div className="p-2 rounded border border-light border-1 bg-transparent">
-
-                            <Row className={styles.emergencyContact}>
-                                <Col md={3}><label><strong>Emergency Contact</strong></label></Col>
-                                <Col md={3}>
-                                    <InputField
-                                        name="emergencyContactFirstName"
-                                        label="First Name:"
-                                        placeholder=""
-                                        error={errors.emergencyContactFirstName}
-                                        disabled={!isEditing}
-                                    />
-                                </Col>
-
-                                <Col md={3}>
-                                    <InputField
-                                        name="emergencyContactLastName"
-                                        label="Last Name:"
-                                        placeholder=""
-                                        error={errors.emergencyContactLastName}
-                                        disabled={!isEditing}
-                                    />
-                                </Col>
-
-                                <Col md={3}>
-                                    <div>
-                                        <label htmlFor="emergencyContactNumber">Phone Number:</label>
-                                        <Field
-                                        type="number"
-                                        id="emergencyContactNumber"
-                                        name="emergencyContactNumber"
-                                        component={PhoneNumberInput}
-                                        placeholder="(123) 456-7890"
-                                        disabled={!isEditing}
-                                        />
-                                    </div>
-                                </Col>
-                            </Row>
-                        </div> */}
                         <Row>
+                            {/* Tab Sections */}
                             <div className="{styles.tabsContainer}">
                                 <Tabs >
                                     <TabList >
@@ -679,7 +564,7 @@ export default function FullIntakeForm({client_id, userId, getToken, isEditMode 
                                         />
                                     </TabPanel>
                                     <TabPanel>
-                                        
+
                                     {/* Legal Notes Tab */}
                                         <LegalNotesPartition
                                             notesData={notesData}
