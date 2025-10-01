867030984048147005f367910beea5b6d898aabe
Saansouk <mick_sana@hotmail.com>
2025-10-01 09:57:32 -0500

    Report page is rendering on application
    Found out that you cannot hava layout.tsx in the folder structure

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import UserHome from "../user-home/page"
@@ -5,7 +6,11 @@ import UserHome from "../user-home/page"
 export default function ReportPage() {
     return (
         <UserHome>
-            <div>This is a report page</div>
+            <div className="min-h-screen bg-gray-100 p-6">
+                <div className="max-w-7xl mx-auto">
+                    <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">This is a report page</h1>
+                </div>
+            </div>
         </UserHome>
     );
 }
