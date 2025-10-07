537bda53024470753939049620245396af88f806
Saansouk <mick_sana@hotmail.com>
2025-10-07 11:17:04 -0500

    Data fetching is working

---
+++ components/data-collection/data-display.tsx
@@ -0,0 +1,58 @@
+import supabase from "../../src/app/lib/supabase";
+import { useEffect, useState } from "react";
+
+type PageProps = {tableName: string};
+
+export const DataDisplay = (tableName: PageProps) => {
+    const [fetchError, setFetchError] = useState<string | null>(null)
+    const [items, setItems] = useState<string[] | number[]| null>(null)
+ 
+    useEffect(() => {
+
+        const fetchItems = async () => {
+            const { data, error } = await supabase
+            .from(`${tableName.tableName}`)
+            .select()
+
+            if (error) {
+                setFetchError('Could not fetch the table')
+                setItems(null)
+                console.log(error)
+
+            }
+            if (data) {
+                setItems(data)
+                setFetchError(null)
+                console.log("working")
+            }
+        }
+
+        fetchItems()
+    }, [tableName])
+
+    if (!Array.isArray(items)) return null;
+    return (
+        <table>
+            {fetchError && (<p>{fetchError}</p>)}
+            <thead>
+                <tr>
+                    {Object.keys(items[0]).map((key) => (
+                        <th key={key}>{key}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {items.map((item, idx) => (
+                    <tr key={idx}>
+                        {Object.values(item).map((value, i) => (
+                            <td key={i}>{value}</td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+
+}
+
+export default DataDisplay;
---
+++ src/app/data-collection/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import UserHome from "../user-home/page"
+import DataDisplay from "../../../components/data-collection/data-display"
+
+
+export default function ReportPage() {
+
+
+
+    return (
+        <UserHome>
+            <div className="min-h-screen bg-gray-100 p-6">
+                <div className="max-w-7xl mx-auto">
+                    {/* This is the main heading */}
+                    <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Data</h1>
+
+                    <DataDisplay
+                        tableName="Advocates"
+                    />
+
+                    <DataDisplay
+                        tableName="Clients"
+                    />
+                    
+                </div>
+            </div>
+        </UserHome>
+    );
+}
