8f5aafb57f7a4f24dc2ca75149b27efa4bc3cd16
RRC-Rohit Kumar <rkumar3@academic.rrc.ca>
2025-10-15 08:38:27 -0500

    Removed the dummy data and reused the advocates-table component to fetch advocates data

--- components/report/report-preview.js
+++ components/report/report-preview.js
@@ -1,5 +1,6 @@
 "use client";
 
+import AdvocatsTableData from "./advocates-table";
 import React from "react";
 
 const ReportPreview = ({ onClose, onDownload }) => {
@@ -34,43 +35,8 @@ const ReportPreview = ({ onClose, onDownload }) => {
 
         {/* Modal content */}
         <div className="overflow-y-auto text-center flex-grow">
-          <div className="my-8 space-y-4 text-gray-700">
-            <h2 className="mb-6 text-2xl font-bold text-center" >Client Info:</h2>
-            <p>
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 
-             1500s, when an unknown printer took a galley of type and scrambled it 
-             to make a type specimen book. It has survived not only five centuries.
-            </p>
-            <p>
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <h2 className="mb-6 text-2xl font-bold text-center" >Client contact Info:</h2>
-            <p>
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 
-             1500s, when an unknown printer took a galley of type and scrambled it 
-             to make a type specimen book. It has survived not only five centuries.
-            </p>
-            <p>
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 
-             1500s, when an unknown printer took a galley of type and scrambled it 
-             to make a type specimen book. It has survived not only five centuries.
-            </p>
-            <p>
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 
-             1500s, when an unknown printer took a galley of type and scrambled it 
-             to make a type specimen book. It has survived not only five centuries.
-            </p>
-            <p>
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 
-             1500s, when an unknown printer took a galley of type and scrambled it 
-             to make a type specimen book. It has survived not only five centuries.
-            </p>
-          </div>
+          <AdvocatsTableData />
+          
         </div>
 
         {/* Modal footer */}
