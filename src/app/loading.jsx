665b7f3185a185c680bfad3b108aaa9c64ebecb1
Dkananowicz2 <dkananowicz2@academic.rrc.ca>
2026-02-12 11:28:47 -0600

    fixed styling issues with loading logo

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,7 @@ import Logo from "../../public/logo.png";
 export default function LoadingPage() {
     return (
         <div className="flex mt-32 text-2xl flex-col items-center justify-center space-y-3">
-            <Image src={Logo} alt="logo" />
+            <Image src={Logo} alt="logo" className="bg-black" />
             <p>Loading...</p>
         </div>
     );
