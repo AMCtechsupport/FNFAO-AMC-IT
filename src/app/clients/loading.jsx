5a7ee513f06b0312515b789143dfd3fe8fbfb3f3
Dkananowicz2 <dkananowicz2@academic.rrc.ca>
2026-02-12 11:57:38 -0600

    Moved loading pages to where they matter, removed root loading page and put 3 in admin, client list, and logs respectivly.

---
+++ src/app/clients/loading.jsx
@@ -0,0 +1,7 @@
+export default function LoadingPageClients() {
+    return (
+        <div className="flex mt-32 text-2xl justify-center animate-pulse">
+            <p>Loading Clients...</p>
+        </div>
+    );
+}
