5a6a5c6a9fefbf5b57965a612b3fd8b55ccacee5
Dkananowicz2 <dkananowicz2@academic.rrc.ca>
2026-02-12 11:14:02 -0600

    Added basic loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Logo from "../../public/logo.png";
+
+export default function LoadingPage() {
+    return (
+        <div className="flex mt-32 text-2xl flex-col items-center justify-center space-y-3">
+            <Image src={Logo} alt="logo" />
+            <p>Loading...</p>
+        </div>
+    );
+}
