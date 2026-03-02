72f02766d54a8bce13e684a4c9afcd5592a440bd
Dkananowicz2 <dkananowicz2@academic.rrc.ca>
2026-03-02 10:40:53 -0600

    Added placeholders for creating advocates in profile page

--- components/link-advocate.js
+++ components/link-advocate.js
@@ -87,6 +87,7 @@ const LinkAdvocate = () => {
             id="firstName"
             type="text"
             value={firstName}
+            placeholder="Advocate's First Name"
             onChange={(e) => {
               setFirstName(e.target.value);
               if (success) setSuccess(null); // Clear success message when user starts typing
@@ -107,6 +108,7 @@ const LinkAdvocate = () => {
             id="lastName"
             type="text"
             value={lastName}
+            placeholder="Advocate's Last Name"
             onChange={(e) => {
               setLastName(e.target.value);
               if (success) setSuccess(null); // Clear success message when user starts typing
@@ -127,6 +129,7 @@ const LinkAdvocate = () => {
             id="email"
             type="email"
             value={email}
+            placeholder="e.g., name@example.com"
             onChange={(e) => {
               setEmail(e.target.value);
               if (success) setSuccess(null); // Clear success message when user starts typing
