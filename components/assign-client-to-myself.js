75919545b0686094dd4acd026d573d26e07a2841
Dkananowicz2 <dkananowicz2@academic.rrc.ca>
2026-03-23 09:26:57 -0500

    Filled more logic into the assign-client-to-myself.js file

--- components/assign-client-to-myself.js
+++ components/assign-client-to-myself.js
@@ -3,9 +3,26 @@
 import { updateClientStatus } from "./client-active";
 import { useState, useEffect, useRef } from "react";
 
-export default function AssignAdvocate() {
-  const [message, setMessage] = useState("");
-  const [showPopup, setShowPopup] = useState(false);
-  const [isAssigned, setIsAssigned] = useState(false);
+import supabase from "../../lib/supabase";
 
+export default async function AssignAdvocateToMyself({ clientToAssign }) {
+    let advocate = null;
+    let advocateError = null;
+
+    try {
+    advocate = await getAdvocateProfile();
+    } catch (err) {
+    console.error("Error fetching advocate:", err.message);
+    advocateError = err.message || "Could not load advocate profile";
+    }
+
+    let advocateId = advocate.advocate_id;
+
+    const { error: autoassignError } = await supabase
+      .from("Assigned Advocates")
+      .insert([{ clientToAssign, advocateId }]);
+
+    if (autoassignError) {
+        throw autoassignError;
+    }
 }
